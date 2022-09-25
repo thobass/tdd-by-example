c52b74bba7eaa8134ff812a4533bd5cbbfddde55
BASSET, Thomas <thomas.basset@cgi.com>
2022-09-25 18:57:10 +0200

    Multi currency money. Closes #2

---
+++ src/main/java/rocks/basset/Dollar.java
@@ -0,0 +1,14 @@
+package rocks.basset;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier){
+        this.amount *= multiplier;
+    }
+}
